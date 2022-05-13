cc0eaa7a9b38fadca0e9ebfe1b41baf8fbd4d5c3
martinlouiee <47809683+martinlouiee@users.noreply.github.com>
2022-05-13 20:02:35 +0800

    Create utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1004,7 +1004,7 @@ def plot_evolution_results(hyp):  # from utils.utils import *; plot_evolution_re
 
 def plot_results_overlay(start=0, stop=0):  # from utils.utils import *; plot_results_overlay()
     # Plot training results files 'results*.txt', overlaying train and val losses
-    s = ['train', 'train', 'train', 'Precision', 'mAP@0.5', 'val', 'val', 'val', 'Recall', 'F1']  # legends
+    s = ['train', 'train', 'train', 'Precision', 'mAP', 'val', 'val', 'val', 'Recall', 'F1']  # legends
     t = ['GIoU', 'Objectness', 'Classification', 'P-R', 'mAP-F1']  # titles
     for f in sorted(glob.glob('results*.txt') + glob.glob('../../Downloads/results*.txt')):
         results = np.loadtxt(f, usecols=[2, 3, 4, 8, 9, 12, 13, 14, 10, 11], ndmin=2).T
@@ -1030,7 +1030,7 @@ def plot_results(start=0, stop=0, bucket='', id=()):  # from utils.utils import 
     fig, ax = plt.subplots(2, 5, figsize=(12, 6))
     ax = ax.ravel()
     s = ['GIoU', 'Objectness', 'Classification', 'Precision', 'Recall',
-         'val GIoU', 'val Objectness', 'val Classification', 'mAP@0.5', 'F1']
+         'val GIoU', 'val Objectness', 'val Classification', 'mAP', 'F1']
     if bucket:
         os.system('rm -rf storage.googleapis.com')
         files = ['https://storage.googleapis.com/%s/results%g.txt' % (bucket, x) for x in id]
